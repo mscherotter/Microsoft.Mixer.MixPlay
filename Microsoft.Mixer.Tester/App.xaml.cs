060c6e5e9d6d402cb3c34f2ae01249315d3546e4
Michael S. Scherotter <mischero@microsoft.com>
2019-08-11 11:33:39 -0700

    Updated target package

--- Microsoft.Mixer.Tester/App.xaml.cs
+++ Microsoft.Mixer.Tester/App.xaml.cs
@@ -40,6 +40,8 @@ namespace InteractiveUWPTester
         /// <param name="e">Details about the launch request and process.</param>
         protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
+            if (e == null) throw new ArgumentNullException(nameof(e));
+
             Frame rootFrame = Window.Current.Content as Frame;
 
             // Do not repeat app initialization when the Window already has content,
